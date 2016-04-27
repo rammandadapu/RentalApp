f0e1fd1908b5bbe9ee6eb10c5bd66d4e68d7fff6
ram.mandadapu <ram.mandadapu@servicenow.com>
2016-04-26 23:25:24 -0700

    Added sendMail component

--- RentalAppSer/app.js
+++ RentalAppSer/app.js
@@ -1,11 +1,16 @@
 var express = require('express')
 	, http = require('http')
+	, nodeMailer = require('nodemailer')
+	, bodyParser = require('body-parser')
 	, index = require('./routes/index');
 
 var app = express();
+app.use(bodyParser.json());
 
 //service
 app.get('/', index.helloWorld);
+app.post('/sendMail', index.sendMail);
+
 
 http.createServer(app).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
