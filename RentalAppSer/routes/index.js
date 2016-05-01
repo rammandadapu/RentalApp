33c3a3ccf5bdb4aeb7955b3c6e0998184a041d23
ram.mandadapu <ram.mandadapu@servicenow.com>
2016-05-01 11:28:06 -0700

    removed mail code from index route

--- RentalAppSer/routes/index.js
+++ RentalAppSer/routes/index.js
@@ -1,36 +1,12 @@
 /**
  * http://usejsdoc.org/
  */
-var nodemailer = require('nodemailer');
+
 
 
 exports.helloWorld = function(req, res) {
 	res.end("Hello World");
-}
+};
 
-exports.sendMail = function(req, res) {
-	var smtpTransport = nodemailer.createTransport("SMTP", {
-		service : "Gmail",
-		auth : {
-			user : "cmpe277maps@gmail.com",
-			pass : "Helloworld@123"
-		}
-	});
 
-	var mailOptions = {
-		to : req.body.to,
-		subject : req.body.subject,
-		text : req.body.text
-	}
-	console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(error, response) {
-		if (error) {
-			console.log(error);
-			res.end("error");
-		} else {
-			console.log("Message sent: " + response.message);
-			res.end("sent");
-		}
-	});
-}
 
