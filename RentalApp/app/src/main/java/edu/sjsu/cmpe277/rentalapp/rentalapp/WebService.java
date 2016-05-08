3c4f0e2edbbc711f4b312ad2d0b540dcc971c069
divya.chittimalla <chdivya.as20@gmail.com>
2016-05-08 15:37:55 -0700

    removed merge conflicts

--- RentalApp/app/src/main/java/edu/sjsu/cmpe277/rentalapp/rentalapp/WebService.java
+++ RentalApp/app/src/main/java/edu/sjsu/cmpe277/rentalapp/rentalapp/WebService.java
@@ -4,25 +4,16 @@ package edu.sjsu.cmpe277.rentalapp.rentalapp;
  * Created by divya.chittimalla on 5/4/16.
  */
 
-import android.content.Context;
+import android.util.Log;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONObject;
-import org.scribe.builder.ServiceBuilder;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
-import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
 import java.io.IOException;
-import java.net.URL;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-
 import java.net.HttpURLConnection;
 
 import edu.sjsu.cmpe277.rentalapp.pojo.Property;
@@ -37,6 +28,7 @@ public class WebService {
     private static final String LOCAL_URL = "http://127.0.0.1:1337/";
 
     public String searchProperties(String keyword, String location, String priceLow, String priceHigh, String condo, String apartment, String house, String townhouse) {
+        try{
         OAuthRequest request = new OAuthRequest(Verb.GET, SERVER_URL+"searchtest");
         request.addQuerystringParameter("keyword", keyword);
         request.addQuerystringParameter("pricelow", priceLow);
@@ -50,33 +42,54 @@ public class WebService {
         Response response = request.send();
         System.out.println(response.getBody());
         return response.getBody();
+        }
+        catch (RuntimeException runTimeException){
+            Log.e("Server con Failed",runTimeException.getMessage());
+            return "";
+        }
+        catch (Exception ex){
+            //Failed to connect to server
+            Log.e("Server con Failed",ex.getMessage());
+            return "";
+        }
     }
 
 
     /***
-     *
      * @param property
      * @return true if server response code is 200
-     *         false if server resposne code is equla to 200
+     * false if server resposne code is equla to 200
      */
-    public boolean postProperty(Property property){
-        OAuthRequest request = new OAuthRequest(Verb.POST, SERVER_URL+"postproperty");
-        ObjectMapper objectMapper=new ObjectMapper();
-        String requestBody=null;
+    public boolean postProperty(Property property) {
         try {
-            requestBody= objectMapper.writeValueAsString(property);
-        } catch (IOException e) {
-            e.printStackTrace();
+            OAuthRequest request = new OAuthRequest(Verb.POST, SERVER_URL + "postproperty");
+            ObjectMapper objectMapper = new ObjectMapper();
+            String requestBody = null;
+            try {
+                requestBody = objectMapper.writeValueAsString(property);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            request.addBodyParameter("post", requestBody);
+            Response response = request.send();
+            return response.getCode() == 201;
         }
-        request.addPayload(requestBody);
-        Response response=request.send();
-        return response.getCode()==200;
+        catch (RuntimeException runTimeException){
+            Log.e("Server con Failed",runTimeException.getMessage());
+            return false;
+        }
+        catch (Exception ex){
+            //Failed to connect to server
+            Log.e("Server con Fail",ex.getMessage());
+            return false;
+        }
+
     }
 
 
 
     public String getPropertyDetails(String _id) {
-        String requestStr = "http://10.0.2.2:1337/property/"+_id;
+        String requestStr = "http://10.0.2.2:1337/property/" + _id;
         OAuthRequest request = new OAuthRequest(Verb.GET, requestStr);
         //Dummy code until real API is available - START
         //String response = "{\"_id\":\"572835524255d0944af1c63d\",\"address\":{\"line1\":\"1 S Market St Apt 502\",\"city\":\"San Jose\",\"state\":\"CA\",\"zip\":\"95113\"},\"type\":\"house\",\"roomsNo\":3,\"bathNo\":2,\"size\":1440,\"price\":5560,\"phone\":\"(238)-434-676\",\"email\":\"test@mail.com\",\"desc\":\"Large spacious apartment. Club house available for free all the time. Pet friendly. Smoke free including patio area.\"}";
