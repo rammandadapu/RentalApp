4bf1675258b54cf08820dca7ba1c50e292dbfaab
rammandadapu <ramakrishna.mandadapu@sjsu.edu>
2016-05-07 23:24:16 -0700

    add route to post data

--- RentalAppSer/routes/fileupload.js
+++ RentalAppSer/routes/fileupload.js
@@ -1,9 +1,35 @@
 /**
  * http://usejsdoc.org/
  */
+var constants = require('./constants');
+var mongodb = require('mongodb');
+var mongoUrl=constants.MongoURL;
+var MongoClient = mongodb.MongoClient;
 
 exports.upload=function(req,res){
-	console.log(req.files);
-	console.log(req.body);
-	res.end("ok");
+	MongoClient.connect(mongoUrl, function (err, db) {
+		  if (err) {
+		    console.log('Unable to connect to the mongoDB server. Error:', err);
+		  } else {		 
+		    console.log('Connection established to', mongoUrl);
+		    var collection = db.collection('property');		    		   
+
+		    var newPost  = JSON.parse(req.body.post);	
+		    console.log(newPost);
+		    //console.log(newPost.post);
+		   // res.send();
+		    //console.log(newPost)	 
+		    collection.insert(newPost,function (err, result) {
+		      if (err) {
+		        console.log(err);
+		      } else{
+		    	  res.status(201);
+		    	  res.send("OK");
+		      } 
+		      //Close connection
+		      db.close();
+		    });
+		  }
+		});
+	
 };
