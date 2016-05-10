f7ceb52c69c5587aadef1364eeb1f156dbb9d828
divya.chittimalla <chdivya.as20@gmail.com>
2016-05-09 18:18:20 -0700

    Fixed a warning and filter UI

--- RentalApp/app/src/main/java/edu/sjsu/cmpe277/rentalapp/rentalapp/NavActivity.java
+++ RentalApp/app/src/main/java/edu/sjsu/cmpe277/rentalapp/rentalapp/NavActivity.java
@@ -55,44 +55,46 @@ public class NavActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();*/
 
 
-                //code for notification START
-                NotificationCompat.Builder mBuilder =
-                        new NotificationCompat.Builder(getApplicationContext())
-                                .setSmallIcon(R.drawable.ic_find_in_page_24dp)
-                                .setContentTitle("My notification")
-                                .setContentText("Hello World!");
-// Creates an explicit intent for an Activity in your app
-                Intent resultIntent = new Intent(getApplicationContext(), NavActivity.class);
+                    //code for notification START
+                    NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(getApplicationContext())
+                                    .setSmallIcon(R.drawable.ic_find_in_page_24dp)
+                                    .setContentTitle("My notification")
+                                    .setContentText("Hello World!");
+    // Creates an explicit intent for an Activity in your app
+                    Intent resultIntent = new Intent(getApplicationContext(), NavActivity.class);
 
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-                TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
-// Adds the back stack for the Intent (but not the Intent itself)
-                stackBuilder.addParentStack(NavActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-                stackBuilder.addNextIntent(resultIntent);
-                PendingIntent resultPendingIntent =
-                        stackBuilder.getPendingIntent(
-                                0,
-                                PendingIntent.FLAG_UPDATE_CURRENT
-                        );
-                mBuilder.setContentIntent(resultPendingIntent);
-                NotificationManager mNotificationManager =
-                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-                mNotificationManager.notify(1, mBuilder.build());
-                //code for notification END
-            }
-        });
+    // The stack builder object will contain an artificial back stack for the
+    // started Activity.
+    // This ensures that navigating backward from the Activity leads out of
+    // your application to the Home screen.
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+    // Adds the back stack for the Intent (but not the Intent itself)
+                    stackBuilder.addParentStack(NavActivity.class);
+    // Adds the Intent that starts the Activity to the top of the stack
+                    stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent =
+                            stackBuilder.getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                            );
+                    mBuilder.setContentIntent(resultPendingIntent);
+                    NotificationManager mNotificationManager =
+                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    // mId allows you to update the notification later on.
+                    mNotificationManager.notify(1, mBuilder.build());
+                    //code for notification END
+                }
+            });
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
